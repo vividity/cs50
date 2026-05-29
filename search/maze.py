54e54264fe9e8134186109feb8dabdbc453d1ac3
vividity <noelwspencer@gmail.com>
2026-05-29 16:02:40 +0200

    dfs algorythm

--- search/maze.py
+++ search/maze.py
@@ -1,3 +1,4 @@
+from mailbox import NotEmptyError
 import sys
 from pathlib import Path
 
@@ -6,7 +7,6 @@ class Maze:
         self.filename = filename
         self.walls = []
    
-
         filepath = Path(__file__).parent / filename
 
         with open(filepath, "r", encoding="utf8") as f:
@@ -26,7 +26,7 @@ class Maze:
                 elif char == "B":
                     self.goal = State(row,col)
                 elif char == "#":
-                    self.walls.append(State(row,col))
+                    self.walls.append((row,col))
                 col += 1
             row -= 1
 
@@ -35,34 +35,75 @@ class State:
         self.row = row
         self.col = col
 
-       
+    def __eq__(self, other) -> bool:
+        return self.row == other.row and self.col == other.col
+    
+    def __hash__(self) -> int:
+        return hash((self.row, self.col))
+
+    def __repr__(self):
+        return f"({self.row}, {self.col})"
 
 class Node:
-    def __init__(self, node, parent) -> None:
-        self.node = node
-        self.parent = parent
+    def __init__(self, state, maze, parent_node = None) -> None:
+        
+        self.state = state
+        self.parent_node = parent_node
+        self.maze = maze
         self.neighbors = []
-        
-    def evaluate_node(self):
-        if State(self.row - 1, self.col) not in m.walls \
-        and (self.row - 1) > 0:
-            self.neighbors.append(State(self.row - 1, self.col))
 
-        if State(self.row + 1, self.col) not in m.walls \
-        and (self.row + 1) < self.height:
-            self.neighbors.append(State(self.row + 1, self.col))
-
-        if State(self.row, self.col + 1) not in m.walls \
-        and (self.col + 1) > 0:
-            self.neighbors.append(State(self.row, self.col - 1))
-              
-        if State(self.row - 1, self.col + 1) not in m.walls \
-        and (self.col + 1) > 0:
-            self.neighbors.append(State(self.row, self.col + 1))    
+        row = self.state.row
+        col = self.state.col
         
-               
+        if (row - 1, col) not in self.maze.walls \
+        and (row - 1) >= 0 \
+        and (self.parent_node is None or State(row - 1, col) != self.parent_node.state):
+            self.neighbors.append(State(row - 1, col))
+
+        if (row + 1, col) not in self.maze.walls \
+        and (row + 1) <= self.maze.height \
+        and (self.parent_node is None or State(row + 1, col) != self.parent_node.state):
+            self.neighbors.append(State(row + 1, col))
+
+        if (row, col + 1) not in self.maze.walls \
+        and (col + 1) >= 0 \
+        and (self.parent_node is None or State(row, col + 1) != self.parent_node.state):
+            self.neighbors.append(State(row, col + 1))
+
+        if (row, col - 1) not in self.maze.walls \
+        and (col - 1) >= 0 \
+        and (self.parent_node is None or State(row, col - 1) != self.parent_node.state):
+            self.neighbors.append(State(row, col - 1))    
+        
+class Search:
+    def __init__(self, maze) -> None:
+        self.maze = maze
+
+        frontier = [Node(maze.start, maze)]
+
+        explored = set()
+
+        while frontier:
+            next_node = frontier.pop()
+            explored.add(next_node.state)
+            
+            
+            new_neighbors = next_node.neighbors
+
+            for i in new_neighbors:
+                if i not in explored:
+                    frontier.append(Node(i, maze, next_node))   
+
+            if next_node.state == maze.goal:
+                path = []
+                while next_node:
+                    path.append(next_node.state)
+                    next_node = next_node.parent_node
+                path.reverse()
+                print(path)
+                return
+        
     
 m = Maze("maze1.txt")
-print(f"Start is ({m.start.row}, {m.start.col})")
-print(f"Goal is ({m.goal.row}, {m.goal.col})")
-print(m.walls[1].row, m.walls[1].col)
+
+Search(m)
